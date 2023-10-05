089c516009770a359d0991e06e182b87f6a7dc63
RamzyAR7 <rameya683@gmail.com>
2023-10-05 22:32:24 +0300

    add v101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - the Entery point
+ *
+ * Return: complete successfully
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+
+	return (1);
+}
