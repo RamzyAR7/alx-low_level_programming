8bc0b1b6a41b31dcb9faa1352031c4541139b544
RamzyAR7 <rameya683@gmail.com>
2023-10-05 21:23:40 +0300

    v5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the Entery point
+ *
+ * Return: complate successfully
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
