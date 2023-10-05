1810d9a52c409bb71500b4d5a68cf7ca3a2becc9
RamzyAR7 <rameya683@gmail.com>
2023-10-05 21:09:37 +0300

    v4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entary point
+ *
+ * Return: successfully complate
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
