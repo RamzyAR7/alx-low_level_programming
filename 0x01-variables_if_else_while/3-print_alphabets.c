e3f6c48a929bf559f0e25fa906564439a807af37
RamzyAR10 <ramzyar010277@gmail.com>
2023-10-08 14:14:10 +0300

    add v3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alawys 0 (success)
+*/
+int main(void)
+{
+	int low_alpha = 97;
+	int up_alpha = 65;
+
+	while (low_alpha <= 122)
+	{
+		putchar(low_alpha);
+		low_alpha++;
+	}
+	while (up_alpha <= 90)
+	{
+		putchar(up_alpha);
+		up_alpha++;
+	}
+	putchar("\n");
+
+	return (0);
+}
