9ccc62a65c3ea295df3d89c530b1bf5cfed65455
RamzyAR7 <rameya683@gmail.com>
2023-10-11 02:27:44 +0300

    add v104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,24 +7,31 @@
  */
 int main(void)
 {
-	unsigned int i;
 	unsigned long a1 = 0, a2 = 0, b1 = 1, b2 = 2;
-	unsigned long carry = 0, temp1, temp2;
+	unsigned long b1_temp, b2_temp, carry = 0;
 
 	printf("%lu, %lu", b1, b2);
 
-	for (i = 3; i <= 98; i++)
+	for (unsigned int i = 3; i <= 98; i++)
 	{
-		temp1 = b1;
-		temp2 = b2;
+		b1_temp = b1;
+		b2_temp = b2;
+
+		/* Calculating Fibonacci for the lower part */
 		b2 = (b1 + b2) % 1000000000;
-		b1 = a1 + a2 + ((temp1 + temp2) / 1000000000);
+		b1 = a1 + a2 + ((b1_temp + b2_temp) / 1000000000);
 
-		carry = (a1 + a2 + carry > a1) ? 0 : 1;
-		a2 += a1 + carry;
-		a1 = (temp1 + temp2) / 1000000000;
+		/* Calculating Fibonacci for the higher part */
+		a2 = (a1 + a2 + carry) % 1000000000;
+		a1 = (b1_temp + b2_temp) / 1000000000;
 
-		if (a2 > 0)
+		/* Checking for carry */
+		if (a1 + a2 + carry > a1)
+			carry = 0;
+		else
+			carry = 1;
+
+		if (a2 > 0 || a1 > 0)
 			printf(", %lu%lu", a2, b2);
 		else
 			printf(", %lu", b2);
