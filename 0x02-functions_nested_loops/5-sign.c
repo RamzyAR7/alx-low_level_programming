ff18ae166bba4da8b18b2c680183339fa6a0fcd8
RamzyAR7 <rameya683@gmail.com>
2023-10-10 21:24:17 +0300

    add v5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - function that prints the sign of a number.
+ * @n: the character to check
+ * by ramzy
+ * Return: 0
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+
+
+}
