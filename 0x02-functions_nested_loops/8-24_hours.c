932b4c02fec054ecd6ffb4229b7b1447a21e935e
RamzyAR7 <rameya683@gmail.com>
2023-10-10 22:49:17 +0300

    add v8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,26 +14,18 @@ void jack_bauer(void)
 		while (y < 60)
 		{
 
-			if (x < 10)
-			{
-				_putchar('0');
-			}
-
-			_putchar(x + '0');
+			_putchar(x  / 10 + '0');
+			_putchar(x  % 10 + '0');
 			_putchar(':');
-
-			if (y < 10)
-			{
-				_putchar('0');
-			}
-
-			_putchar(y + '0');
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
 			_putchar('\n');
 
 			y++;
 		}
 
 		x++;
+		y = 0;
 	}
 
 }
