e5c5047cef5f248f5f4af9dea7460abd06351d9c
RamzyAR7 <rameya683@gmail.com>
2023-10-10 23:56:49 +0300

    add v11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,25 @@
 */
 void print_to_98(int n)
 {
-	while (n < 99)
+	if (n > 98)
 	{
-		printf("%d", n);
-
-		if (n != 98)
+		while (n > 98)
 		{
-			_putchar(',');
-			_putchar(' ');
-		}
+			printf(", %d", n);
 
-		n++;
+			n--;
+		}
 	}
+	else
+	{
+		while (n < 98)
+		{
+			printf(", %d", n);
+
+			n++;
+		}
+	}
+
+	printf("98\n");
+
 }
