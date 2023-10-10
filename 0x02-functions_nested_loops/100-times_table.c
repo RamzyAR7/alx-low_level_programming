a080b29e69e0fea3e56e95cc744beb2933a9e6f9
RamzyAR7 <rameya683@gmail.com>
2023-10-11 01:37:28 +0300

    add v100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0.
- * @n : take num as input
+ * @n: take num as input
  * by ramzy
  */
 void print_times_table(int n)
@@ -15,40 +15,40 @@ void print_times_table(int n)
 	{
 		while (x <= n)
 		{
+			y = 0;
 			while (y <= n)
 			{
-
-				num = x * y;
-
-				if (y != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				if (num >= 10)
-				{
-					_putchar(num / 10 + '0');
-					_putchar(num % 10 + '0');
-				}
-				else if (num < 10 && y != 0)
-				{
-					_putchar(' ');
-					_putchar(num  % 10 + '0');
-				}
-				else
-				{
-					_putchar(num  % 10 + '0');
-				}
-
-				y++;
-			}
-
-			y = 0;
-			_putchar('\n');
-			x++;
-
-		}
-
-	}
+                num = x * y;
+                if (y == 0)
+                {
+                    _putchar('0');
+                }
+                else
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    if (num < 10)
+                    {
+                        _putchar(' ');
+                    }
+                    if (num < 100)
+                    {
+                        _putchar(' ');
+                    }
+                    if (num >= 100)
+                    {
+                        _putchar(num / 100 + '0');
+                    }
+                    if (num >= 10)
+                    {
+                        _putchar((num / 10) % 10 + '0');
+                    }
+                    _putchar(num % 10 + '0');
+                }
+                y++;
+            }
+            _putchar('\n');
+            x++;
+        }
+    }
 }
