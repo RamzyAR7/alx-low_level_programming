867fdc0088262e6cfe6770cd8ad85e38e80d0b43
RamzyAR7 <rameya683@gmail.com>
2023-10-11 02:24:55 +0300

    add v104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,63 +1,35 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * get_num_length - Returns the length of digits in a number.
- * @n: The number
- *
- * Return: The number of digits
- */
-int get_num_length(unsigned long n)
-{
-	int length = 0;
-
-	if (n == 0)
-		return (1);
-	while (n)
-	{
-		n /= 10;
-		length++;
-	}
-	return (length);
-}
-
-/**
- * print_fibonacci - Prints the first 98 Fibonacci numbers.
- */
-void print_fibonacci(void)
-{
-	int count, num_zeros_to_print;
-	unsigned long f1 = 1, f2 = 2, sum, max_val = 100000000;
-	unsigned long f1_overflow = 0, f2_overflow = 0, sum_overflow = 0;
-
-	for (count = 1; count <= 98; ++count)
-	{
-		if (f1_overflow > 0)
-			printf("%lu", f1_overflow);
-		num_zeros_to_print = get_num_length(max_val) - 1 - get_num_length(f1);
-		for (int i = 0; i < num_zeros_to_print; i++)
-			printf("0");
-		printf("%lu", f1);
-		sum = (f1 + f2) % max_val;
-		sum_overflow = f1_overflow + f2_overflow + (f1 + f2) / max_val;
-		f1 = f2;
-		f1_overflow = f2_overflow;
-		f2 = sum;
-		f2_overflow = sum_overflow;
-		if (count != 98)
-			printf(", ");
-		else
-			printf("\n");
-	}
-}
-
-/**
- * main - Program entry point.
+ * main - Print the first 98 Fibonacci numbers.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	print_fibonacci();
+	unsigned int i;
+	unsigned long a1 = 0, a2 = 0, b1 = 1, b2 = 2;
+	unsigned long carry = 0, temp1, temp2;
+
+	printf("%lu, %lu", b1, b2);
+
+	for (i = 3; i <= 98; i++)
+	{
+		temp1 = b1;
+		temp2 = b2;
+		b2 = (b1 + b2) % 1000000000;
+		b1 = a1 + a2 + ((temp1 + temp2) / 1000000000);
+
+		carry = (a1 + a2 + carry > a1) ? 0 : 1;
+		a2 += a1 + carry;
+		a1 = (temp1 + temp2) / 1000000000;
+
+		if (a2 > 0)
+			printf(", %lu%lu", a2, b2);
+		else
+			printf(", %lu", b2);
+	}
+
+	printf("\n");
 	return (0);
 }
