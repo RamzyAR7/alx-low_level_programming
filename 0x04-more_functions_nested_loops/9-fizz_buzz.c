2b1115a87fd9d15d3e96e90109351bb1c481e4ae
RamzyAR7 <rameya683@gmail.com>
2023-10-12 20:00:20 +0300

    add v9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Fizz-Buzz test
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		printf("%d ", i);
+
+	}
+
+
+	return (0);
+}
