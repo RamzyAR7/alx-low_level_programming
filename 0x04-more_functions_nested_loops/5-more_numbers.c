e6fc5fc5f49b483d72efaab1de3788be5c8c1ac8
RamzyAR7 <rameya683@gmail.com>
2023-10-12 16:10:24 +0300

    add v5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * more_numbers - a function that prints 10 times the numbers,
+ *  from 0 to 14, followed by a new line.
+ * by ramzy
+*/
+void more_numbers(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; i < 15; x++)
+		{
+			if (x > 9)
+			{
+				_putchar(x / 10 + '0');
+				_putchar(x % 10 + '0');
+			}
+			else
+			{
+				_putchar(x + '0');
+			}
+
+		}
+
+		_putchar('\n');
+	}
+}
