094c8b1776696601811fb04fbebfba5774cfc4fd
RamzyAR7 <rameya683@gmail.com>
2023-10-19 01:26:48 +0300

    add v6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,22 +10,31 @@ char *cap_string(char *a)
 	int i;
 	int x;
 
-	char arr[] = {',', ';', '.', '!', '?', '"', '('
-	, ')', '{', '}', '\n', '\t', '-', ' '};
+	char arr[] = {',', ';', '.', '!', '?'
+	, '"', '(', ')', '{', '}', '\n', '\t', '-', ' '};
 
 	if (a[0] >= 'a' && a[0] <= 'z')
 	{
-		a[0] -= 32;
 	}
 
-	for (i = 1; a[i] != '\0'; i++) /* Start from 1 */
+	for (i = 1; a[i] != '\0'; i++)
 	{
-		for (x = 0; arr[x] != '\0'; x++)
+
+		if (a[i] >= 'a' && a[i] <= 'z')
 		{
-			if (a[i - 1] == arr[x] && a[i] >= 'a' && a[i] <= 'z')
+			if (a[i] == a[0])
 			{
-				a[i] -= 32;
-				break; /* Break out of the inner loop once a match is found */
+				a[0] -= 32;
+			}
+			else
+			{
+				for (x = 0; arr[x] != '\0'; x++)
+				{
+					if (a[i - 1] == arr[x])
+					{
+						a[i] -= 32;
+					}
+				}
 			}
 		}
 	}
