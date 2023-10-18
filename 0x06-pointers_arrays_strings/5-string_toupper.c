ea067bdb712f9ed06ff8e756e649562efa8da0d4
RamzyAR7 <rameya683@gmail.com>
2023-10-18 23:28:20 +0300

    add v5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *string_toupper - a function that changes
+ *  all lowercase letters of a string to uppercase
+ * @a: array of char
+ * Return: Always char
+ * by ramzy
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] -= 32;
+		}
+		else
+		{
+			a[i];
+		}
+	}
+
+	return (a);
+}
