12b5a4038ba9187fb2591453628f3bf4fa058214
RamzyAR7 <rameya683@gmail.com>
2023-10-18 21:27:55 +0300

    add v3

--- 3-strcmp.c
+++ 3-strcmp.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
- * _strcmp - a function that compares two strings
- * @s1: char of pointers
- * @s2: char of pointer
- * Return: Always the compares int
- * by ramzy
+ * _strcmp - compare 2 string
+ *
+ * @s1:string
+ * @s2:strmp
+ *
+ * Return:int
  */
+
 int _strcmp(char *s1, char *s2)
 {
 	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
