9711d10be8a7358b2f6afe2a7466f0410670b370
RamzyAR7 <rameya683@gmail.com>
2023-10-18 22:26:37 +0300

    add v3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,8 +14,8 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return (s1[i] - s2[i]); /*for return if + , -*/
 		}
 	}
-	return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]); /* for *s1 = *s2 ---> 0 */
 }
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - a function that reverses
+ *  the content of an array of integers.
+ * @a: int of pointer
+ * @n: is the number of elements of the array
+ * by ramzy
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	i = n - 1;
+
+	while (i >= 0)
+	{
+		printf("%d", a[i]);
+
+		if (i != 0)
+		{
+			printf(", ");
+		}
+
+		i--;
+	}
+
+	printf("\n");
+}
