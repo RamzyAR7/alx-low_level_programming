fc207b7a786a46f3e0570fa59dad2357b4e5cab1
RamzyAR7 <rameya683@gmail.com>
2023-10-19 01:12:01 +0300

    add v6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * *cap_string - a function that capitalizes all words of a string.
+ * @a: char of array
+ * Return: Always char
+ * by ramzy
+ */
+char *cap_string(char *a)
+{
+	int i;
+	int x;
+
+	char arr[] = {',', ';', '.', '!', '?', '"'
+	, '(', ')', '{', '}', '\n', '\t', '-', ' '};
+
+	if (a[0] >= 'a' && a[0] <= 'z')
+	{
+		a[0] -= 32;
+	}
+
+	for (i = 1; a[i] != '\0'; i++)
+	{
+
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			for (x = 0; arr[x] != '\0'; x++)
+			{
+				if (a[i - 1] == arr[x])
+				{
+					a[i] -= 32;
+				}
+			}
+		}
+	}
+
+	return (a);
+}
