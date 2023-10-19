ecdbfff000d26351b88bfa43e90001738bdbc24a
RamzyAR7 <rameya683@gmail.com>
2023-10-20 02:05:02 +0300

    add v100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,21 +7,20 @@
  *
  * Return: char a
  */
-
 char *rot13(char *a)
 {
 	int i, x;
 	char c1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char c2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (i = 0; a[i] != '\0'; i++)
+	for (i = 0; *(a + i); i++)
 	{
 		for (x = 0; x < 52; x++)
 		{
-			if (a[i] == c1[x])
+			if (c1[x] == *(a + i))
 			{
-				a[i] = c2[x];
-
+				*(a + i) = c2[x];
+				break;
 			}
 		}
 	}
