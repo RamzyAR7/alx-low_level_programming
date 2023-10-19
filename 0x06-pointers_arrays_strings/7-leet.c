a3d453249888256eaddb17edfa7b08c217dfe338
RamzyAR7 <rameya683@gmail.com>
2023-10-19 03:09:27 +0300

    add v6

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *leet - a function that encodes a string into 1337.
+ * @a: char of array
+ * Return: Always char
+ * by ramzy
+ */
+char *leet(char *a)
+{
+	int i;
+	int x;
+
+	char n[] = "4433007711";
+	char c[] = "aAeEoOtTiL";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (x = 0; c[x] != '\0'; x++)
+		{
+			if (a[i] == c[x])
+			{
+				a[i] = n[x];
+			}
+		}
+	}
+
+	return (a);
+}
