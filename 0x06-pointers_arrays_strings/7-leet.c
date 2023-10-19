a25d0f1fba3444dd3d0cbd2876b2e5714fa8f52b
RamzyAR7 <rameya683@gmail.com>
2023-10-19 03:35:39 +0300

    add v7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,14 +10,16 @@ char *leet(char *a)
 	int i;
 	int x;
 
-	char n[] = "4433007711";
-	char c[] = "aAeEoOtTiL";
+	char n[] = "43071";
+
+	char c1[] = "AEOTL";
+	char c2[] = "aeotl";
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		for (x = 0; c[x] != '\0'; x++)
+		for (x = 0; x < 5; x++)
 		{
-			if (a[i] == c[x])
+			if (a[i] == c1[x] || a[i] == c1[x])
 			{
 				a[i] = n[x];
 			}
