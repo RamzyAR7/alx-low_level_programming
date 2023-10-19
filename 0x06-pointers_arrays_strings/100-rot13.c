ffc74fb943665302d34a8c5b73b64181379b1d5d
RamzyAR7 <rameya683@gmail.com>
2023-10-19 21:43:22 +0300

    add v100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string using rot13.
+ *
+ * @a: char of array
+ *
+ * Return: char s
+ */
+
+char *rot13(char *a)
+{
+	int i, x;
+	char c1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char c2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (x = 0; x < 52; x++)
+		{
+			if (c1[x] == a[i])
+			{
+				a[i] = c2[x];
+
+			}
+		}
+	}
+
+	return (a);
+}
