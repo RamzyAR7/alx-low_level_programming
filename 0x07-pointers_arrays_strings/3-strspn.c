aba941aa263a42d81421282012cf2b6da1e95a16
RamzyAR7 <rameya683@gmail.com>
2023-10-24 00:10:12 +0300

    add v3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,24 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int count = 0;
 	int index_s;
 	int index_accept;
+	int found;
 
 	for (index_s = 0; s[index_s] != '\0'; index_s++)
 	{
+		found = 0;
+
 		for (index_accept = 0; accept[index_accept] !=  '\0'; index_accept++)
 		{
 			if (s[index_s] == accept[index_accept])
 			{
 				count++;
+				found = 1;
+				break;
 			}
+		}
+		if (!found)
+		{
+			break;
 		}
 	}
 
