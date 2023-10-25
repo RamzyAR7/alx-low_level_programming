81430fd3d55d70d713232b652a6b6fa74766a2d0
RamzyAR7 <rameya683@gmail.com>
2023-10-26 02:57:42 +0300

    add v2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,12 +2,12 @@
 /**
  * _strlen_recursion - a function that returns the length of a string.
  * @s: array of char
- * @count: the length
  * Return: the length of string
  * by ramzy
  */
 int _strlen_recursion(char *s)
 {
+
 	if (*s == '\0')
 	{
 		return (0);
