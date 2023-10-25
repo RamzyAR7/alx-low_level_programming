8fbe701e898b93a209279d5971b4a2134ef38028
RamzyAR7 <rameya683@gmail.com>
2023-10-26 01:57:36 +0300

    add v0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,6 +10,7 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\0');
+		_putchar('\n');
 	}
 	else
 	{
@@ -17,5 +18,4 @@ void _puts_recursion(char *s)
 		s++;
 		_puts_recursion(s);
 	}
-	_putchar('\n');
 }
