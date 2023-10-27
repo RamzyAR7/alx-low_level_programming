b78a8686a02038204ca2347f9fb747fb3b6b6171
RamzyAR7 <rameya683@gmail.com>
2023-10-27 17:57:06 +0200

    add v0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * @argc: the number of command
+ * @argv: the array of string
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
