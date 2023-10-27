2dee2cdb7d77fa4134ea6760793640c30b168394
RamzyAR7 <rameya683@gmail.com>
2023-10-27 19:33:53 +0200

    add v100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,43 +11,39 @@ int main(int argc, char *argv[])
 	int N_coins;
 	int coins = 0;
 
-	if (argc == 2)
-	{
-		N_coins = atoi(argv[1]);
-
-		if (N_coins > 0)
-		{
-			while (N_coins >= 0)
-			{
-				if (N_coins >= 25)
-					N_coins -= 25;
-
-				else if (N_coins >= 10)
-					N_coins -= 10;
-
-				else if (N_coins >= 5)
-					N_coins -= 5;
-
-				else if (N_coins >= 2)
-					N_coins -= 2;
-
-				else if (N_coins >= 1)
-					N_coins -= 1;
-				else
-					break;
-				coins++;
-			}
-		}
-		else
-		{
-			printf("0\n");
-			return (0);
-		}
-	}
-	else
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
+	}
+
+	N_coins = atoi(argv[1]);
+
+	if (N_coins < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (N_coins >= 0)
+	{
+		if (N_coins >= 25)
+			N_coins -= 25;
+
+		else if (N_coins >= 10)
+			N_coins -= 10;
+
+		else if (N_coins >= 5)
+			N_coins -= 5;
+
+		else if (N_coins >= 2)
+			N_coins -= 2;
+
+		else if (N_coins >= 1)
+			N_coins -= 1;
+		else
+			break;
+		coins++;
 	}
 
 	printf("%d\n", coins);
