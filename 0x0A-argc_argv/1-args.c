09b8c7cb6f136f32f36e1019cd5cb1763d2ba239
RamzyAR7 <rameya683@gmail.com>
2023-10-27 18:09:57 +0200

    add v1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * @argc: the number of command
+ * @argv: the array of string
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
