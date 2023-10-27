0b91e411034ba7303afb53b6beba27b1b654fa4f
RamzyAR7 <rameya683@gmail.com>
2023-10-27 18:42:22 +0200

    add v4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the entry point
+ * @argc: the number of command
+ * @argv: the array of string
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+	char *a;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (*a = argv[i]; *a; a++)
+		{
+			if (*a < '0' || *a > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
