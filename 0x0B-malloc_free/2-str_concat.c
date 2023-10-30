c1a774e15823e8aa6e33fa2453840b52c6b52c28
RamzyAR7 <rameya683@gmail.com>
2023-10-30 12:14:02 +0200

    add v2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,40 +19,31 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-
 	/* to count the length of s1 */
 	while (s1[s1_l])
 	{
 		s1_l++;
 	}
-
 	/* to count the length of s2 */
 	while (s2[s2_l])
 	{
 		s2_l++;
 	}
-
 	/* to have the all length to malloc */
 	length = s1_l + s2_l;
-
 	str = malloc((length + 1) * sizeof(char));
-
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < s1_l; i++)
 	{
 		str[i] = s1[i];
 	}
-
-	for (i = s1_l; i < length; i++) /* i = s1 to complate the concate
-	 and i < length be cause length = s1_l + s2_l */
+	for (i = s1_l; i < length; i++)
 	{
 		str[i] = s2[i - s1_l]; /* to start from index 0 in the s1 array */
 	}
-
 	str[i] = '\0'; /* the last elemnt str[length] = NULL */
 
 	return (str);
