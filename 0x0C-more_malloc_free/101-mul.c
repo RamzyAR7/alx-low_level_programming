e2b012a60b26c810efae64c5e2ced83c699d192e
RamzyAR7 <rameya683@gmail.com>
2023-11-03 01:09:14 +0200

    add v101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,146 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-int is_number(char *str);
-void print_error_and_exit(void);
-long int multiply(char *num1, char *num2);
-
-/**
- * main - Entry point, multiplies two numbers
- * @argc: Argument count
- * @argv: Argument vector
- *
- * Return: 0 on success, 98 on failure
- */
-int main(int argc, char *argv[])
-{
-	long int result;
-
-	if (argc != 3)
-	{
-		print_error_and_exit();
-	}
-
-	if (!is_number(argv[1]) || !is_number(argv[2]))
-	{
-		print_error_and_exit();
-	}
-
-	result = multiply(argv[1], argv[2]);
-	printf("%ld\n", result);
-
-	return (0);
-}
-
-/**
- * is_number - Checks if a string is a number
- * @str: The string to be checked
- *
- * Return: 1 if string is a number, 0 otherwise
- */
-int is_number(char *str)
-{
-	int i = 0;
-
-	if (str[0] == '\0')
-		return (0);
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (!isdigit(str[i]))
-			return (0);
-	}
-
-	return (1);
-}
-
-/**
- * print_error_and_exit - Prints an error and exits
- */
-void print_error_and_exit(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * multiply - Multiplies two numbers given as strings
- * @num1: The first number as a string
- * @num2: The second number as a string
- *
- * Return: The result of the multiplication
- */
-long int multiply(char *num1, char *num2)
-{
-	return (atol(num1) * atol(num2));
-}
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
- * is_all_digits - determines if the string consists of only digits
- * @arg: string to be checked
- * Return: 0 if all are digits, otherwise 1.
+ * _isdigit - tells if the string consists of digits
+ * @n: pointer to current item in argument
+ * Return: return 0 if all digits, 1 if not all digits.
  */
-int is_all_digits(char *arg)
+int _isdigit(char *n)
 {
-	int idx = 0;
+	int i;
 
-	while (arg[idx])
+	i = 0;
+	while (n[i])
 	{
-		if (arg[idx] < '0' || arg[idx] > '9')
+		if (n[i] >= '0' && n[i] <= '9')
+			i++;
+		else
 			return (1);
-		idx++;
 	}
 	return (0);
 }
-
 /**
- * string_to_int - converts a string of digits to the integer they represent
- * @str: string to be converted
- * Return: integer representation of the string.
+ * _atoi - converts a string of ascii digits to the values they represent
+ * @c: pointer to the source string
+ * Return: value of digits
  */
-int string_to_int(char *str)
+int _atoi(char *c)
 {
-	int idx = 0, value = 0;
+	int i, result;
 
-	while (str[idx])
+	i = result = 0;
+	while (c[i])
 	{
-		value = value * 10 + (str[idx] - '0');
-		idx++;
-	}
-	return (value);
-}
-
-/**
- * primary_func - the main function of the program
- * @arg_count: number of arguments
- * @arg_values: array of argument strings
- * Return: 0 on success, 98 on error.
- */
-int primary_func(int arg_count, char *arg_values[])
-{
-	int idx;
-
-	malloc();
-
-	if (arg_count != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (idx = 1; idx < arg_count; idx++)
-	{
-		if (is_all_digits(arg_values[idx]))
+		if (c[i] >= '0' && c[i] <= '9')
 		{
-			printf("Error\n");
-			exit(98);
+			result *= 10;
+			result += (c[i] - '0');
 		}
+		i++;
 	}
-
-	return (0);
+	return (result);
 }
 /**
  * main - main function call
@@ -150,5 +48,21 @@ int primary_func(int arg_count, char *arg_values[])
  */
 int main(int argc, char *argv[])
 {
-	return (primary_func(argc, argv));
+	int i;
+
+	malloc();
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (_isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	return (0);
 }
