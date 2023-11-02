7f77d8d04396308ad8525c92277233da0e3251b4
RamzyAR7 <rameya683@gmail.com>
2023-11-03 01:28:27 +0200

    add v101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -46,10 +46,12 @@ int is_number(char *str)
 	if (str[0] == '\0')
 		return (0);
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i])
 	{
 		if (!isdigit(str[i]))
 			return (0);
+
+		i++;
 	}
 
 	return (1);
