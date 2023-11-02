b62b56af7735a82589a9394b2c2679d4a411d612
RamzyAR7 <rameya683@gmail.com>
2023-11-03 00:56:17 +0200

    add v101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,116 +1,77 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
+
+int is_number(char *str);
+void print_error_and_exit(void);
+long int multiply(char *num1, char *num2);
 
 /**
- * check_for_digit - Checks if a string consists only of digits
- * @number: The string to check
+ * main - Entry point, multiplies two numbers
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Return: 1 if only digits, 0 otherwise
+ * Return: 0 on success, 98 on failure
  */
-int check_for_digit(char *number)
+int main(int argc, char *argv[])
 {
-	while (*number)
+	long int result;
+
+	if (argc != 3)
 	{
-		if (*number < '0' || *number > '9')
-			return (0);
-		number++;
+		print_error_and_exit();
 	}
+
+	if (!is_number(argv[1]) || !is_number(argv[2]))
+	{
+		print_error_and_exit();
+	}
+
+	result = multiply(argv[1], argv[2]);
+	printf("%ld\n", result);
+
+	return (0);
+}
+
+/**
+ * is_number - Checks if a string is a number
+ * @str: The string to be checked
+ *
+ * Return: 1 if string is a number, 0 otherwise
+ */
+int is_number(char *str)
+{
+	int i = 0;
+
+	if (str[0] == '\0')
+		return (0);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i]))
+			return (0);
+	}
+
 	return (1);
 }
 
 /**
- * multiply - Multiplies two numbers represented as strings
- * @num1: The first number string
- * @num2: The second number string
+ * print_error_and_exit - Prints an error and exits
  */
-void multiply(char *num1, char *num2)
+void print_error_and_exit(void)
 {
-	int length1 = string_length(num1), length2 = string_length(num2);
-	int totalLength = length1 + length2, i, j, k, product, carry;
-	int *result = allocate_and_initialize(totalLength, sizeof(int));
-
-	if (!result)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = length1 - 1; i >= 0; i--)
-	{
-		for (j = length2 - 1; j >= 0; j--)
-		{
-			product = (num1[i] - '0') * (num2[j] - '0');
-			carry = result[i + j + 1] + product;
-			result[i + j] += carry / 10;
-			result[i + j + 1] = carry % 10;
-		}
-	}
-
-	k = 0;
-	while (k < totalLength - 1 && result[k] == 0)
-		k++;
-	for (; k < totalLength; k++)
-		printf("%d", result[k]);
-	printf("\n");
-	free(result);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * string_length - Calculates the length of a string recursively
- * @s: The string to calculate the length
+ * multiply - Multiplies two numbers given as strings
+ * @num1: The first number as a string
+ * @num2: The second number as a string
  *
- * Return: The length of the string
+ * Return: The result of the multiplication
  */
-int string_length(char *s)
+long int multiply(char *num1, char *num2)
 {
-	if (*s)
-		return (1 + string_length(s + 1));
-	return (0);
-}
-
-/**
- * allocate_and_initialize - Allocates and zeros memory
- * @count: Number of elements
- * @size: Size of each element
- *
- * Return: Pointer to allocated memory
- */
-void *allocate_and_initialize(unsigned int count, unsigned int size)
-{
-	unsigned int total_size = count * size, index;
-	void *allocated_memory;
-	char *pointer;
-
-	if (count == 0 || size == 0)
-		return (NULL);
-
-	allocated_memory = malloc(total_size);
-
-	if (allocated_memory != NULL)
-	{
-		pointer = (char *)allocated_memory;
-		for (index = 0; index < total_size; index++)
-			pointer[index] = 0;
-	}
-	return (allocated_memory);
-}
-
-/**
- * main - Entry point, multiplies two numbers passed as arguments
- * @argc: Argument count
- * @argv: Argument vector
- *
- * Return: 0 on success, 98 on error
- */
-int main(int argc, char *argv[])
-{
-	if (argc != 3 || !check_for_digit(argv[1]) || !check_for_digit(argv[2]))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	multiply(argv[1], argv[2]);
-	return (0);
+	return (atol(num1) * atol(num2));
 }
