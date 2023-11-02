6a54f10faa7a42bd720c7e215c59ee4667c6762c
RamzyAR7 <rameya683@gmail.com>
2023-11-03 01:30:26 +0200

    add v101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,79 +1,109 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-int is_number(char *str);
-void print_error_and_exit(void);
-long int multiply(char *num1, char *num2);
+#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point, multiplies two numbers
- * @argc: Argument count
- * @argv: Argument vector
+ * contains_only_digits - determines if the string has only digit characters
+ * @str: string to examine
  *
- * Return: 0 on success, 98 on failure
+ * Return: 1 if only digits, 0 otherwise
  */
-int main(int argc, char *argv[])
+int contains_only_digits(char *str)
 {
-	long int result;
+	int idx = 0;
 
-	if (argc != 3)
+	while (str[idx])
 	{
-		print_error_and_exit();
-	}
-
-	if (!is_number(argv[1]) || !is_number(argv[2]))
-	{
-		print_error_and_exit();
-	}
-
-	result = multiply(argv[1], argv[2]);
-	printf("%ld\n", result);
-
-	return (0);
-}
-
-/**
- * is_number - Checks if a string is a number
- * @str: The string to be checked
- *
- * Return: 1 if string is a number, 0 otherwise
- */
-int is_number(char *str)
-{
-	int i = 0;
-
-	if (str[0] == '\0')
-		return (0);
-
-	while (str[i])
-	{
-		if (!isdigit(str[i]))
+		if (str[idx] < '0' || str[idx] > '9')
 			return (0);
-
-		i++;
+		idx++;
 	}
-
 	return (1);
 }
 
 /**
- * print_error_and_exit - Prints an error and exits
+ * get_length - computes the length of a string
+ * @str: string to measure
+ *
+ * Return: length of the string
  */
-void print_error_and_exit(void)
+int get_length(char *str)
+{
+	int len = 0;
+
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * display_error - shows error message and exits program
+ */
+void display_error(void)
 {
 	printf("Error\n");
 	exit(98);
 }
 
 /**
- * multiply - Multiplies two numbers given as strings
- * @num1: The first number as a string
- * @num2: The second number as a string
+ * main - multiplies two positive numbers
+ * @args_count: argument count
+ * @args: argument array
  *
- * Return: The result of the multiplication
+ * Return: 0 on success
  */
-long int multiply(char *num1, char *num2)
+int main(int args_count, char *args[])
 {
-	return (atol(num1) * atol(num2));
+	char *num1, *num2;
+	int num1_len, num2_len, total_len, idx, overflow, val1, val2, *product, started = 0;
+
+	num1 = args[1], num2 = args[2];
+	if (args_count != 3 || !contains_only_digits(num1) || !contains_only_digits(num2))
+		display_error();
+
+	num1_len = get_length(num1);
+	num2_len = get_length(num2);
+	total_len = num1_len + num2_len + 1;
+
+	product = malloc(sizeof(int) * total_len);
+	if (!product)
+		return (1);
+
+	for (idx = 0; idx <= num1_len + num2_len; idx++)
+		product[idx] = 0;
+
+	for (num1_len = num1_len - 1; num1_len >= 0; num1_len--)
+	{
+		val1 = num1[num1_len] - '0';
+		overflow = 0;
+
+		for (num2_len = get_length(num2) - 1; num2_len >= 0; num2_len--)
+		{
+			val2 = num2[num2_len] - '0';
+			overflow += product[num1_len + num2_len + 1] + (val1 * val2);
+			product[num1_len + num2_len + 1] = overflow % 10;
+			overflow /= 10;
+		}
+
+		if (overflow)
+			product[num1_len + num2_len + 1] += overflow;
+	}
+
+	for (idx = 0; idx < total_len - 1; idx++)
+	{
+		if (product[idx])
+			started = 1;
+
+		if (started)
+			_putchar(product[idx] + '0');
+	}
+
+	if (!started)
+		_putchar('0');
+
+	_putchar('\n');
+	free(product);
+	return (0);
 }
