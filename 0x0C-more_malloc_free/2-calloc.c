5999bb1a07743eebb4af99722e880a84e6dd69de
RamzyAR7 <rameya683@gmail.com>
2023-11-02 16:07:26 +0200

    add v2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,17 +10,23 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *p;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	p = (int *)malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 	{
 		return (NULL);
+	}
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		p[i] = 0;
 	}
 
 	return (p);
