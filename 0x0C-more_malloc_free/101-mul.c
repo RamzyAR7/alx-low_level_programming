6ff65780d69d5162b599757bf6c691dcbb89a6d2
RamzyAR7 <rameya683@gmail.com>
2023-11-03 02:10:24 +0200

    add v101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,36 +3,18 @@
 #include "main.h"
 
 /**
- * contains_only_digits - determines if the string has only digit characters
- * @str: string to examine
- *
- * Return: 1 if only digits, 0 otherwise
+ * validate_input - validates input and gets string length
+ * @str: input string
+ * Return: length if only digits, -1 otherwise
  */
-int contains_only_digits(char *str)
-{
-	int idx = 0;
-
-	while (str[idx])
-	{
-		if (str[idx] < '0' || str[idx] > '9')
-			return (0);
-		idx++;
-	}
-	return (1);
-}
-
-/**
- * get_length - computes the length of a string
- * @str: string to measure
- *
- * Return: length of the string
- */
-int get_length(char *str)
+int validate_input(char *str)
 {
 	int len = 0;
 
 	while (str[len])
 	{
+		if (str[len] < '0' || str[len] > '9')
+			return (-1);
 		len++;
 	}
 	return (len);
@@ -48,55 +30,60 @@ void display_error(void)
 }
 
 /**
+ * initialize_product - initializes product array
+ * @len: length of the product array
+ * Return: initialized product array
+ */
+int *initialize_product(int len)
+{
+	int *product = malloc(sizeof(int) * len);
+
+	if (!product)
+		exit(98);
+	for (int idx = 0; idx < len; idx++)
+		product[idx] = 0;
+	return (product);
+}
+
+/**
  * main - multiplies two positive numbers
- * @args_count: argument count
- * @args: argument array
- *
+ * @argc: argument count
+ * @argv: argument array
  * Return: 0 on success
  */
-int main(int args_count, char *args[])
+int main(int argc, char *argv[])
 {
-	char *num1, *num2;
-	int num1_len, num2_len, total_len, idx, overflow, val1, val2, *product, started = 0;
+	int idx, *product, started = 0, num1_len, num2_len, total_len;
+	char *num1 = argv[1], *num2 = argv[2];
 
-	num1 = args[1], num2 = args[2];
-	if (args_count != 3 || !contains_only_digits(num1) || !contains_only_digits(num2))
+	if (argc != 3)
 		display_error();
-
-	num1_len = get_length(num1);
-	num2_len = get_length(num2);
+	num1_len = validate_input(num1);
+	if (num1_len == -1)
+		display_error();
+	num2_len = validate_input(num2);
+	if (num2_len == -1)
+		display_error();
 	total_len = num1_len + num2_len + 1;
-	product = malloc(sizeof(int) * total_len);
-	if (!product)
-		return (1);
-
-	for (idx = 0; idx <= num1_len + num2_len; idx++)
-		product[idx] = 0;
-	for (num1_len = num1_len - 1; num1_len >= 0; num1_len--)
-	{
-		val1 = num1[num1_len] - '0';
-		overflow = 0;
-
-		for (num2_len = get_length(num2) - 1; num2_len >= 0; num2_len--)
+	product = initialize_product(total_len);
+	for (--num1_len; num1_len >= 0; num1_len--)
+		for (num2_len--; num2_len >= 0; num2_len--)
 		{
-			val2 = num2[num2_len] - '0';
-			overflow += product[num1_len + num2_len + 1] + (val1 * val2);
+			int overflow = (num1[num1_len] - '0') * (num2[num2_len] - '0');
+
+			overflow += product[num1_len + num2_len + 1];
 			product[num1_len + num2_len + 1] = overflow % 10;
-			overflow /= 10;
+			product[num1_len + num2_len] += overflow / 10;
 		}
 
-		if (overflow)
-			product[num1_len + num2_len + 1] += overflow;
-	}
-	for (idx = 0; idx < total_len - 1; idx++)
+	for (idx = 0; idx < total_len; idx++)
 	{
-		if (product[idx])
-			started = 1;
-
-		if (started)
+		if (product[idx] || started)
+		{
 			_putchar(product[idx] + '0');
+			started = 1;
+		}
 	}
-
 	if (!started)
 		_putchar('0');
 	_putchar('\n');
