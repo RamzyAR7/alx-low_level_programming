c1479cf9469592e24ead8daf9d1ec390496e1268
RamzyAR7 <rameya683@gmail.com>
2023-11-03 02:12:19 +0200

    add v101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,92 +1,126 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * validate_input - validates input and gets string length
- * @str: input string
- * Return: length if only digits, -1 otherwise
+ * check_num - function to check the string for number
+ * @st: string being passed
+ * Return: 1 for number 0 for not
  */
-int validate_input(char *str)
+int check_num(char *st)
 {
-	int len = 0;
+	int a;
 
-	while (str[len])
+	for (a = 0; st[a] != '\0'; a++)
 	{
-		if (str[len] < '0' || str[len] > '9')
-			return (-1);
-		len++;
+		if (st[a] < '0' || st[a] > '9')
+			return (0);
 	}
-	return (len);
+	return (1);
+}
+/**
+ * string_length - calculating string length
+ * @str: string to check
+ * Return: count
+ *
+ */
+unsigned int string_length(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+		a++;
+	return (a);
 }
 
 /**
- * display_error - shows error message and exits program
+ * print_string - function to print string
+ * @st: string to print
+ * Return: none
  */
-void display_error(void)
+void print_string(char *st)
 {
-	printf("Error\n");
-	exit(98);
-}
-
-/**
- * initialize_product - initializes product array
- * @len: length of the product array
- * Return: initialized product array
- */
-int *initialize_product(int len)
-{
-	int *product = malloc(sizeof(int) * len);
-
-	if (!product)
-		exit(98);
-	for (int idx = 0; idx < len; idx++)
-		product[idx] = 0;
-	return (product);
-}
-
-/**
- * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument array
- * Return: 0 on success
- */
-int main(int argc, char *argv[])
-{
-	int idx, *product, started = 0, num1_len, num2_len, total_len;
-	char *num1 = argv[1], *num2 = argv[2];
-
-	if (argc != 3)
-		display_error();
-	num1_len = validate_input(num1);
-	if (num1_len == -1)
-		display_error();
-	num2_len = validate_input(num2);
-	if (num2_len == -1)
-		display_error();
-	total_len = num1_len + num2_len + 1;
-	product = initialize_product(total_len);
-	for (--num1_len; num1_len >= 0; num1_len--)
-		for (num2_len--; num2_len >= 0; num2_len--)
-		{
-			int overflow = (num1[num1_len] - '0') * (num2[num2_len] - '0');
-
-			overflow += product[num1_len + num2_len + 1];
-			product[num1_len + num2_len + 1] = overflow % 10;
-			product[num1_len + num2_len] += overflow / 10;
-		}
-
-	for (idx = 0; idx < total_len; idx++)
-	{
-		if (product[idx] || started)
-		{
-			_putchar(product[idx] + '0');
-			started = 1;
-		}
-	}
-	if (!started)
+	while (*st == '\0')
+		st++;
+	if (*st == '\0')
 		_putchar('0');
+	while (*st == '0')
+		st++;
+	while (*st != '\0')
+	{
+		_putchar(*st);
+		st++;
+	}
 	_putchar('\n');
-	free(product);
+}
+
+/**
+ * _calloc - function for memory
+ * @number: the number
+ * @size: the size
+ * Return: pointer to memory
+ */
+void *_calloc(unsigned int number, unsigned int size)
+{
+	char *p;
+	unsigned int a;
+
+	if (number == 0 || size == 0)
+		return (NULL);
+	p = malloc(number * size);
+	if (p == 0)
+		return (NULL);
+	for (a = 0; a < (number * size); a++)
+		p[a] = 0;
+	return (p);
+}
+
+/**
+ * main - function to multiply
+ * @argc: number of arguments passed
+ * @argv: argument variables
+ * Return: Always zero
+ */
+int main(int argc, char **argv)
+{
+	char *n1, *n2, *multi_res;
+	unsigned int l = 0, l1 = 0, l2 = 0, a, b, t = 0, c = 0, ten = 0;
+
+	if (argc < 3)
+	{
+		print_string("Error");
+		exit(98);
+	}
+	n1 = argv[1];
+	n2 = argv[2];
+	if (!(check_num(n1) && check_num(n2)))
+	{
+		print_string("Error");
+		exit(98);
+	}
+	l1 = string_length(n1);
+	l2 = string_length(n2);
+	l = l1 + l2;
+	multi_res = _calloc(l + 1, sizeof(char *));
+	if (multi_res == 0)
+	{
+		print_string("Error");
+		exit(98);
+	}
+	for (a = 0; a < l1; a++, ten++)
+	{
+		for (c = 0, b = 0; b < l2; b++)
+		{
+			t = (n1[l1 - a - 1] - '0') * (n2[l2 - b - 1] - '0') + c;
+			printf("%u\n", t);
+			if (multi_res[l - b - ten - 1] > 0)
+				t = t + multi_res[l - b - ten - 1] - '0';
+			multi_res[l - b - ten - 1] = t % 10 + '0';
+			c = t / 10;
+		}
+		multi_res[l - b - ten - 1] += c + '0';
+	}
+	print_string(multi_res);
+	free(multi_res);
 	return (0);
 }
