2aedce670c7f329040df6082190d08bc3c962720
RamzyAR7 <rameya683@gmail.com>
2023-11-03 02:18:07 +0200

    add v101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,126 +1,105 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * check_num - function to check the string for number
- * @st: string being passed
- * Return: 1 for number 0 for not
+ * contains_only_digits - determines if the string has only digit characters
+ * @str: string to examine
+ *
+ * Return: 1 if only digits, 0 otherwise
  */
-int check_num(char *st)
+int contains_only_digits(char *str)
 {
-	int a;
+	int idx = 0;
 
-	for (a = 0; st[a] != '\0'; a++)
+	while (str[idx])
 	{
-		if (st[a] < '0' || st[a] > '9')
+		if (str[idx] < '0' || str[idx] > '9')
 			return (0);
+		idx++;
 	}
 	return (1);
 }
+
 /**
- * string_length - calculating string length
- * @str: string to check
- * Return: count
+ * get_length - computes the length of a string
+ * @str: string to measure
  *
+ * Return: length of the string
  */
-unsigned int string_length(char *str)
+int get_length(char *str)
 {
-	int a;
+	int len = 0;
 
-	for (a = 0; str[a] != '\0'; a++)
-		a++;
-	return (a);
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
 }
 
 /**
- * print_string - function to print string
- * @st: string to print
- * Return: none
+ * display_error - shows error message and exits program
  */
-void print_string(char *st)
+void display_error(void)
 {
-	while (*st == '\0')
-		st++;
-	if (*st == '\0')
-		_putchar('0');
-	while (*st == '0')
-		st++;
-	while (*st != '\0')
-	{
-		_putchar(*st);
-		st++;
-	}
-	_putchar('\n');
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * _calloc - function for memory
- * @number: the number
- * @size: the size
- * Return: pointer to memory
+ * main - multiplies two positive numbers
+ * @args_count: argument count
+ * @args: argument array
+ *
+ * Return: 0 on success
  */
-void *_calloc(unsigned int number, unsigned int size)
+int main(int args_count, char *args[])
 {
-	char *p;
-	unsigned int a;
+	char *num1, *num2;
+	int num1_len, num2_len, total_len, idx,
+		overflow, val1, val2, *product, started = 0;
 
-	if (number == 0 || size == 0)
-		return (NULL);
-	p = malloc(number * size);
-	if (p == 0)
-		return (NULL);
-	for (a = 0; a < (number * size); a++)
-		p[a] = 0;
-	return (p);
-}
+	num1 = args[1], num2 = args[2];
+	if (args_count != 3 || !contains_only_digits(num1) || !contains_only_digits(num2))
+		display_error();
 
-/**
- * main - function to multiply
- * @argc: number of arguments passed
- * @argv: argument variables
- * Return: Always zero
- */
-int main(int argc, char **argv)
-{
-	char *n1, *n2, *multi_res;
-	unsigned int l = 0, l1 = 0, l2 = 0, a, b, t = 0, c = 0, ten = 0;
+	num1_len = get_length(num1);
+	num2_len = get_length(num2);
+	total_len = num1_len + num2_len + 1;
+	product = malloc(sizeof(int) * total_len);
+	if (!product)
+		return (1);
+	for (idx = 0; idx <= num1_len + num2_len; idx++)
+		product[idx] = 0;
+	for (num1_len = num1_len - 1; num1_len >= 0; num1_len--)
+	{
+		val1 = num1[num1_len] - '0';
+		overflow = 0;
 
-	if (argc < 3)
-	{
-		print_string("Error");
-		exit(98);
-	}
-	n1 = argv[1];
-	n2 = argv[2];
-	if (!(check_num(n1) && check_num(n2)))
-	{
-		print_string("Error");
-		exit(98);
-	}
-	l1 = string_length(n1);
-	l2 = string_length(n2);
-	l = l1 + l2;
-	multi_res = _calloc(l + 1, sizeof(char *));
-	if (multi_res == 0)
-	{
-		print_string("Error");
-		exit(98);
-	}
-	for (a = 0; a < l1; a++, ten++)
-	{
-		for (c = 0, b = 0; b < l2; b++)
+		for (num2_len = get_length(num2) - 1;
+			 num2_len >= 0; num2_len--)
 		{
-			t = (n1[l1 - a - 1] - '0') * (n2[l2 - b - 1] - '0') + c;
-			printf("%u\n", t);
-			if (multi_res[l - b - ten - 1] > 0)
-				t = t + multi_res[l - b - ten - 1] - '0';
-			multi_res[l - b - ten - 1] = t % 10 + '0';
-			c = t / 10;
+			val2 = num2[num2_len] - '0';
+			overflow += product[num1_len + num2_len + 1] + (val1 * val2);
+			product[num1_len + num2_len + 1] = overflow % 10;
+			overflow /= 10;
 		}
-		multi_res[l - b - ten - 1] += c + '0';
+		if (overflow)
+			product[num1_len + num2_len + 1] += overflow;
 	}
-	print_string(multi_res);
-	free(multi_res);
+	for (idx = 0; idx < total_len - 1; idx++)
+	{
+		if (product[idx])
+			started = 1;
+
+		if (started)
+			_putchar(product[idx] + '0');
+	}
+
+	if (!started)
+		_putchar('0');
+	_putchar('\n');
+	free(product);
 	return (0);
 }
