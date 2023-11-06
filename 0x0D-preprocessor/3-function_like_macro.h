48da83c0cab45efc8255380f1678a42a8998b6c9
RamzyAR7 <rameya683@gmail.com>
2023-11-06 14:48:52 +0200

    add v3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef ABS(x)
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) ((x) < (0) ? (x * -1) : (x))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
-#endif
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
