8fb9326bdafcf94714bfff81b26bfd08a5fb2cc8
RamzyAR7 <rameya683@gmail.com>
2023-11-06 20:48:12 +0200

    add v4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,52 +1,44 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * new_dog - creates a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: pointer to new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog structure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int i, x, y, z;
+	int name_len, owner_len;
 
-	new_dog = malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(dog_t)); /* Allocate memory for new dog */
 	if (new_dog == NULL)
-	{
 		return (NULL);
-	}
-	while (name[i])
-	{
-		i++;
-	}
 
-	new_dog->name = malloc((i + 1) * sizeof(char));
+	name_len = strlen(name);							   /* Calculate length of name */
+	new_dog->name = malloc((name_len + 1) * sizeof(char));
 	if (new_dog->name == NULL)
 	{
-		free(new_dog);
+		free(new_dog); /* Clean up if allocation fails */
 		return (NULL);
 	}
-	for (x = 0; x < i; x++)
-	{
-		new_dog->name[x] = name[x];
-	}
-	new_dog->age = age;
-	while (owner[y])
-	{
-		y++;
-	}
-	new_dog->owner = malloc((y + 1) * sizeof(char));
+	strcpy(new_dog->name, name); /* Copy name to new allocation */
+
+	new_dog->age = age; /* Set age for new dog */
+
+	owner_len = strlen(owner);								 /* Calculate length of owner */
+	new_dog->owner = malloc((owner_len + 1) * sizeof(char));
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog);
+		free(new_dog->name); /* Clean up name if allocation fails */
+		free(new_dog);		 /* Clean up struct if allocation fails */
 		return (NULL);
 	}
-	for (z = 0; z < y; z++)
-	{
-		new_dog->owner[z] = owner[z];
-	}
-	return (new_dog);
+	strcpy(new_dog->owner, owner); /* Copy owner to new allocation */
+
+	return (new_dog); /* Return new dog struct */
 }
