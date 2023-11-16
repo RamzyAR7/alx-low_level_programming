b1d0714a4fc8b6955204f6487090cfa260fbccd8
RamzyAR7 <rameya683@gmail.com>
2023-11-17 01:57:53 +0200

    add v3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-		end->str = _strdup(str);
+		end->str = strdup(str);
 		while (str[counter])
 		{
 			counter++;
@@ -53,47 +53,4 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 
 	return (end);
-}
-/**
- * *_strdup - function returns a pointer to a new string which is
- *  a duplicate of the string
- * @str: array of char
- * Return: the address
- */
-char *_strdup(char *str)
-{
-	char *c;
-	int length = 0;
-	int i;
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-
-	/* to get the length of str */
-	while (str[length])
-	{
-		length++;
-	}
-
-	c = malloc((length + 1) * sizeof(char)); /* +1 this for NULL OP*/
-
-	if (c == NULL)
-	{
-		return (NULL);
-	}
-
-	/**
-	 *  we use length instate of c[i] != '\0'
-	 * because there is no array of c are yet init
-	 */
-	for (i = 0; i < length; i++)
-	{
-		c[i] = str[i]; /* to copy and double the string into c */
-	}
-
-	c[i] = '\0'; /* the last elemnt str[length] = NULL */
-
-	return (c);
 }
