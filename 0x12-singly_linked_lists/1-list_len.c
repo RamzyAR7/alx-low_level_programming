60019fde7b5740151fdfab24d0dca93cc75391e8
RamzyAR7 <rameya683@gmail.com>
2023-11-16 23:45:50 +0200

    add v1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the list_t list to find the number of elements of
+ *
+ * Return: the number of elements in the list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	list_t *ptrNode = h;
+
+	unsigned int counter = 0;
+
+	while (ptrNode)
+	{
+		counter++;
+
+		ptrNode = ptrNode->next;
+	}
+
+	return (counter);
+}
