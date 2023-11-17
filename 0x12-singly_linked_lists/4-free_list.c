915f70653a35a03054e243b90667d738d4bf2cfc
RamzyAR7 <rameya683@gmail.com>
2023-11-17 02:37:16 +0200

    add v4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the list_t list to be freed
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		if (head->str)
+		{
+			free(head->str);
+		}
+		free(head);
+		head = temp;
+	}
+}
