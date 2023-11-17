1e2938200cb0bd01355b9afcd886ce8cd77bea98
RamzyAR7 <rameya683@gmail.com>
2023-11-17 03:05:36 +0200

    add v101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,25 @@
+julien@ubuntu:~/0x00$ nasm -f elf64 1-hello_holberton.asm && gcc -no-pie -std=gnu89 1-hello_holberton.o -o hello
+julien@ubuntu:~/0x00$ ./hello
+Hello, Holberton
+julien@ubuntu:~/0x00$
+```
+```
+julien@ubuntu:~/0x00$ cat 1-hello_holberton.asm
+section .data
+		msg:    db      "Hello, Holberton", 10
+		len:    equ     $ - msg
+section .text
+		global _start
+
+_start:
+		mov     rax, 1
+		mov     rdi, 1
+		mov     rsi, msg
+		mov     rdx, len
+		syscall
+		mov     rax, 60
+		mov     rdi, 0
+		syscall
+julien@ubuntu:~/0x00$
+```
+```
