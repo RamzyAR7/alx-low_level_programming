b2bc711c608b782fd8cd82e5e25526dea5e4a2c2
RamzyAR7 <rameya683@gmail.com>
2023-11-17 03:14:08 +0200

    add v101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,31 +1,22 @@
+julien@ubuntu:~/0x00$ nasm -f elf64 1-hello_holberton.asm && gcc -no-pie -std=gnu89 1-hello_holberton.o -o hello
+julien@ubuntu:~/0x00$ ./hello
+Hello, Holberton
+julien@ubuntu:~/0x00$
+```
+```
+julien@ubuntu:~/0x00$ cat 1-hello_holberton.asm
 section .data
-    hello db "Hello, Holberton", 10  ; 10 is the ASCII code for newline
-    format db "%s", 0
-
+		msg:    db      "Hello, Holberton", 10
+		len:    equ     $ - msg
 section .text
-    global main
-    extern printf  ; You don't need this if you're defining printf below
+		global _start
 
-main:
-    ; Set up arguments for printf
-    mov rdi, hello
-
-    ; Call printf
-    call printf
-
-    ; Exit the program
-    mov rax, 60  ; syscall number for sys_exit
-    xor rdi, rdi ; status 0
-    syscall
-
-section .text
-    ; Dummy printf function for assembly
-    global printf
-    printf:
-        ; Set up syscall parameters
-        mov rax, 1          ; syscall number for sys_write
-        mov rdi, 1          ; file descriptor 1 (stdout)
-        mov rsi, [rsp + 8]  ; message pointer from stack
-        mov rdx, 17         ; message length
-        syscall
-        ret
+_start:
+		mov     rax, 1
+		mov     rdi, 1
+		mov     rsi, msg
+		mov     rdx, len
+		syscall
+		mov     rax, 60
+		mov     rdi, 0
+		syscall
