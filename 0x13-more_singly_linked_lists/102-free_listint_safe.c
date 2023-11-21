6fcc0dca8cbea60e09c70e0598811b579c644672
RamzyAR7 <rameya683@gmail.com>
2023-11-21 19:27:50 +0200

    add v103

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,34 +10,34 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-    size_t counter = 0;
-    listint_t *current_node;
-    listint_t *temp_node;
-    listint_t *checker_node;
+	size_t counter = 0;
+	listint_t *current_node;
+	listint_t *temp_node;
+	listint_t *checker_node;
 
-    if (h == NULL || *h == NULL)
-        return (0);
+	if (h == NULL || *h == NULL)
+		return (0);
 
-    current_node = *h;
-    while (current_node != NULL)
-    {
-        checker_node = *h;
-        while (checker_node != current_node)
-        {
-            if (checker_node == current_node->next)
-            {
-                *h = NULL;
-                return (counter);
-            }
-            checker_node = checker_node->next;
-        }
+	current_node = *h;
+	while (current_node != NULL)
+	{
+		checker_node = *h;
+		while (checker_node != current_node)
+		{
+			if (checker_node == current_node->next)
+			{
+				*h = NULL;
+				return (counter);
+			}
+			checker_node = checker_node->next;
+		}
 
-        temp_node = current_node->next;
-        free(current_node);
-        current_node = temp_node;
-        counter++;
-    }
+		temp_node = current_node->next;
+		free(current_node);
+		current_node = temp_node;
+		counter++;
+	}
 
-    *h = NULL;
-    return (counter);
+	*h = NULL;
+	return (counter);
 }
--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,25 +1,34 @@
 #include "lists.h"
+
 /**
- * find_listint_loop - finds the loop in a linked list
- * @head: pointer to head of linked list
+ * find_listint_loop - Finds the loop in a linked list
+ * @head: Pointer to the head of the linked list
  *
- * Return: address of node where loop starts, or NULL if no loop
+ * Description: This function uses Floyd's Tortoise and Hare algorithm
+ * to detect a loop in a linked list. If a loop is found, it returns
+ * the address of the node where the loop starts, or NULL if there is no loop.
+ * Return: The address of the node where the loop starts, or NULL if no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *current_node, *checker_node;
+	listint_t *tortoise, *hare;
 
-	current_node = head;
-	while (current_node != NULL)
+	tortoise = hare = head;
+	while (hare != NULL && hare->next != NULL)
 	{
-		checker_node = head;
-		while (checker_node != current_node)
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
 		{
-			if (checker_node == current_node->next)
-				return (current_node->next);
-			checker_node = checker_node->next;
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			return (tortoise);
 		}
-		current_node = current_node->next;
 	}
 	return (NULL);
 }
