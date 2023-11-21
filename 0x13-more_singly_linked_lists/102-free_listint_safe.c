65c2ad8a2c4066417d3b9141039fb42b283fc521
RamzyAR7 <rameya683@gmail.com>
2023-11-21 19:33:13 +0200

    add v102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a listint_t linked list
- * @h: double pointer to the head of the linked list
+ * free_listint_safe - Frees a listint_t linked list, even with a loop
+ * @h: Double pointer to the head of the linked list
  *
- * Description: This function can free lists with a loop.
- * It goes through the list only once and sets the head to NULL.
+ * Description: This function frees a listint_t linked list,
+ * even if the list contains a loop. It goes through the list
+ * only once and sets the head to NULL. It breaks the loop
+ * if found.
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t counter = 0;
-	listint_t *current_node;
-	listint_t *temp_node;
-	listint_t *checker_node;
+	listint_t *current_node, *temp;
 
 	if (h == NULL || *h == NULL)
 		return (0);
@@ -21,20 +21,17 @@ size_t free_listint_safe(listint_t **h)
 	current_node = *h;
 	while (current_node != NULL)
 	{
-		checker_node = *h;
-		while (checker_node != current_node)
+		if (current_node->next >= current_node)
 		{
-			if (checker_node == current_node->next)
-			{
-				*h = NULL;
-				return (counter);
-			}
-			checker_node = checker_node->next;
+			temp = current_node->next;
+			current_node->next = NULL;
+			current_node = temp;
+			continue;
 		}
 
-		temp_node = current_node->next;
+		temp = current_node->next;
 		free(current_node);
-		current_node = temp_node;
+		current_node = temp;
 		counter++;
 	}
 
