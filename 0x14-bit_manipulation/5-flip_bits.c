792dc64c190cd4873c35451b98429418e6076854
RamzyAR7 <rameya683@gmail.com>
2023-11-23 12:37:20 +0200

    add v5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,5 +9,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	
+	unsigned int number_flip = 0;
+
+	unsigned long int xor_bits = n ^ m;
+
+	while (xor_bits)
+	{
+		number_flip += xor_bits & 1;
+
+		xor_bits >> 1;
+	}
+
+	return (number_flip);
 }
