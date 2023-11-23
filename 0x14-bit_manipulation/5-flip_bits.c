764349fecfe2725b6e171f0f67e5a87c2e7855d6
RamzyAR7 <rameya683@gmail.com>
2023-11-23 12:41:19 +0200

    add v5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,7 +5,8 @@
  * @n: first number
  * @m: second number
  *
- * Return: number of bits you would need to flip to get from one number to another
+ * Return: number of bits you would need to flip to get
+ * from one number to another
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
