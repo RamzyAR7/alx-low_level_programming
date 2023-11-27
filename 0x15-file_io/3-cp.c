6249d1736cb58a9bf670c383dc2d1803c40569a2
RamzyAR7 <rameya683@gmail.com>
2023-11-27 21:44:13 +0200

    add v3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <fcntl.h>
+#include <sys/stat.h>
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
@@ -15,12 +16,12 @@
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, read_bytes, write_bytes;
-	char buffer[1024];
+	char *buffer;
+	mode_t file_perm;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-
 		exit(97);
 	}
 
@@ -28,29 +29,36 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[1]);
-
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	/* open file_to */
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-			argv[2]);
-
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
 		exit(99);
+	}
+
+	/* allocate buffer */
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		close(fd_from);
+		close(fd_to);
+		exit(98);
 	}
 
 	/* read file_from */
 	read_bytes = read(fd_from, buffer, 1024);
 	if (read_bytes == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[1]);
-
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		close(fd_from);
+		close(fd_to);
 		exit(98);
 	}
 
@@ -58,29 +66,16 @@ int main(int argc, char *argv[])
 	write_bytes = write(fd_to, buffer, read_bytes);
 	if (write_bytes == -1 || write_bytes != read_bytes)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-			argv[2]);
-
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
+		close(fd_from);
+		close(fd_to);
 		exit(99);
 	}
 
-	/* close file_from */
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-			fd_from);
-
-		exit(100);
-	}
-
-	/* close file_to */
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
-			fd_to);
-
-		exit(100);
-	}
+	free(buffer);
+	close(fd_from);
+	close(fd_to);
 
 	return (0);
 }
