595e636979bd3e5722e9faf3c6a8f94e58fc9023
RamzyAR7 <rameya683@gmail.com>
2024-01-12 00:21:44 +0200

    add v5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *current = head;
 	unsigned int x = 0;
 
-	while (x != index  && current)
+	while (current && x != index)
 	{
 		current = current->next;
 		x++;
