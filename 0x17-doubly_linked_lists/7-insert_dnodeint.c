84d51256c89af180944f0daf7d7a79ce0572c715
RamzyAR7 <rameya683@gmail.com>
2024-01-12 02:52:09 +0200

    add v7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -40,18 +40,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-
 	if (current == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->next = current->next;
 	new_node->prev = current;
 	if (current->next != NULL)
 		current->next->prev = new_node;
 	current->next = new_node;
-
 	return (new_node);
 }
