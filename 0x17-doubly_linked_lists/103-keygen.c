6e4075235b2519b480bf84d5c18068934e290ac3
RamzyAR7 <rameya683@gmail.com>
2024-01-12 02:34:45 +0200

    add v103

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,11 +3,11 @@
 #include <time.h>
 
 /**
- * f4 - Find the biggest ASCII value from the username.
- * @usrn: Username
- * @len: Length of username
+ * f4 - finds the biggest number
  *
- * Return: Biggest ASCII value.
+ * @usrn: username
+ * @len: length of username
+ * Return: the biggest number
  */
 int f4(char *usrn, int len)
 {
@@ -22,8 +22,9 @@ int f4(char *usrn, int len)
 	{
 		if (ch < usrn[vch])
 			ch = usrn[vch];
-		vch++;
+		vch += 1;
 	}
+
 	srand(ch ^ 14);
 	rand_num = rand();
 
@@ -31,11 +32,11 @@ int f4(char *usrn, int len)
 }
 
 /**
- * f5 - Calculate a random character based on the username.
- * @usrn: Username
- * @len: Length of username
+ * f5 - multiplies each char of username
  *
- * Return: Random character
+ * @usrn: username
+ * @len: length of username
+ * Return: multiplied char
  */
 int f5(char *usrn, int len)
 {
@@ -43,43 +44,43 @@ int f5(char *usrn, int len)
 	int vch;
 
 	ch = vch = 0;
-	/* Calculate a random character based on the username*/
+
 	while (vch < len)
 	{
-		ch += usrn[vch] * usrn[vch];
-		vch++;
+		ch = ch + usrn[vch] * usrn[vch];
+		vch += 1;
 	}
 
 	return (((unsigned int)ch ^ 239) & 63);
 }
 
 /**
- * f6 - Calculate a random character based on the username.
- * @usrn: Username
+ * f6 - generates a random char
  *
- * Return: Random character
- *
+ * @usrn: username
+ * Return: a random char
+ */
 int f6(char *usrn)
 {
 	int ch;
 	int vch;
 
 	ch = vch = 0;
-	/* Generate a random character based on the username */
-while (vch < *usrn)
-{
-	ch = rand();
-	vch++;
-}
 
-return (((unsigned int)ch ^ 229) & 63);
+	while (vch < *usrn)
+	{
+		ch = rand();
+		vch += 1;
+	}
+
+	return (((unsigned int)ch ^ 229) & 63);
 }
 
 /**
  * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
  *
+ * @argc: arguments count
+ * @argv: arguments vector
  * Return: Always 0
  */
 int main(int argc, char **argv)
@@ -89,52 +90,38 @@ int main(int argc, char **argv)
 	long alph[] = {
 		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
 		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
-		0x723161513346655a, 0x6b756f494b646850};
+		0x723161513346655a, 0x6b756f494b646850 };
+	(void) argc;
 
-	(void)argc;
-
-	/* Calculate the length of the username */
 	for (len = 0; argv[1][len]; len++)
 		;
-
-	/* f1: Calculate the first character of the key */
+	/* ----------- f1 ----------- */
 	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
-
-	/* f2: Calculate the second character of the key */
+	/* ----------- f2 ----------- */
 	ch = vch = 0;
 	while (vch < len)
 	{
-		ch += argv[1][vch];
-		vch++;
+		ch = ch + argv[1][vch];
+		vch = vch + 1;
 	}
 	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
-
-	/* f3: Calculate the third character of the key */
+	/* ----------- f3 ----------- */
 	ch = 1;
 	vch = 0;
 	while (vch < len)
 	{
-		ch = argv[1][vch] *
-			 ch;
-		vch++;
+		ch = argv[1][vch] * ch;
+		vch = vch + 1;
 	}
 	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
-
-	/* f4: Calculate the fourth character of the key */
+	/* ----------- f4 ----------- */
 	keygen[3] = ((char *)alph)[f4(argv[1], len)];
-
-	/* f5: Calculate the fifth character of the key*/
+	/* ----------- f5 ----------- */
 	keygen[4] = ((char *)alph)[f5(argv[1], len)];
-
-	/* f6: Calculate the sixth character of the key */
+	/* ----------- f6 ----------- */
 	keygen[5] = ((char *)alph)[f6(argv[1])];
-
 	keygen[6] = '\0';
-
 	for (ch = 0; keygen[ch]; ch++)
 		printf("%c", keygen[ch]);
-
-	printf("\n");
-
 	return (0);
 }
