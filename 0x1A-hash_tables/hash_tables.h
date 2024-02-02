c4a035d72203447a0dffa8ae7420036a0fbfa326
RamzyAR7 <rameya683@gmail.com>
2024-02-02 08:35:20 +0200

    add v6

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,5 +42,6 @@ int add_new_node(hash_table_t *ht, const char *key,
 				 const char *value, unsigned long int idx);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
