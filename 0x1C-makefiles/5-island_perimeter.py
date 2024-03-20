7d615d2194a519c712fe4705c747bebf1567eb77
RamzyAR7 <rameya683@gmail.com>
2024-03-21 00:38:10 +0200

    add v100

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,5 +5,18 @@ this module for Island Perimeter function
 
 
 def island_perimeter(grid):
-	"""func : Island Perimeter"""
-	
+    """func : Island Perimeter"""
+    colum = len(grid[0])
+    row = len(grid)
+    edg = 0
+    s = 0
+
+    for i in range(row):
+        for j in range(colum):
+            if grid[i][j] == 1:
+                s += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edg += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edg += 1
+    return s * 4 - edg * 2
