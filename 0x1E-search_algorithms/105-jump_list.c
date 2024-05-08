82f0bd3d52fc11a7eea5c86f9182b0c6676ad8e7
RamzyAR7 <rameya683@gmail.com>
2024-05-08 22:32:22 +0300

    add v105

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -14,37 +14,34 @@
 
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t jump, i;
-	listint_t *current, *prev;
+	listint_t *current_node, *old_node;
+	size_t jump = sqrt(size), i = 0;
 
-	if (!list || size == 0)
+	if (!list)
 		return (NULL);
-
-	jump = sqrt(size);
-	current = list;
-	while (current->n < value)
+	current_node = list, old_node = list;
+	while (current_node && current_node->next && current_node->index < size - 1)
 	{
-		prev = current;
-		for (i = 0; current->next && i < jump; i++)
-			current = current->next;
-		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-		if (!current->next)
+		old_node = current_node;
+		i = 0;
+		while (current_node->index < size - 1 && i++ < jump)
+			current_node = current_node->next;
+		printf("Value checked at index [%lu] = [%d]\n",
+			   current_node->index, current_node->n);
+		if (current_node->n >= value)
 			break;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n",
-		   prev->index, current->index);
-	printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-	while (prev->n < value)
+		   old_node->index, current_node->index);
+	while (old_node->index <= current_node->index)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-		if (!prev->next || prev->index == current->index)
-			return (NULL);
-		prev = prev->next;
-	}
-	if (prev->n == value)
-	{
-		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-		return (prev);
+		printf("Value checked at index [%lu] = [%d]\n",
+			   old_node->index, old_node->n);
+		if (old_node->n == value)
+			return (old_node);
+		if (old_node->index == current_node->index)
+			break;
+		old_node = old_node->next;
 	}
 	return (NULL);
 }
